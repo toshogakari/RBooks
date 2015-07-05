04ced3b85648943642186c3e70b3b90d15c051c7
kazuhide maeda <kzhd15med3pub@gmail.com>
2015-07-05 22:26:34 +0900

    implements Search class.

--- lib/rbooks.rb
+++ lib/rbooks.rb
@@ -1,5 +1,43 @@
-require "rbooks/version"
+require './rbooks/version'
+require 'net/https'
+require 'json'
 
-module Rbooks
-  # Your code goes here...
+module RBooks
+  HOST = 'app.rakuten.co.jp'
+  PATH = '/services/api/BooksBook/Search/20130522'
+
+  class Search
+    attr_accessor :params
+
+    def initialize(app_id)
+      @app_id = app_id
+      @params = {}
+    end
+
+    def get
+      yield @params if block_given?
+
+      url = make_query
+
+      http = Net::HTTP.new(HOST, 443)
+      http.use_ssl = true
+      req = Net::HTTP::Get.new(url)
+      res = http.request(req)
+
+      JSON.parse(res.body)
+    end
+
+    private
+
+    def make_query
+      url = PATH
+      url += '?format=json'
+
+      @params.each do |key, value|
+        url += "&#{key}=#{value}"
+      end
+
+      url += "&applicationId=#{@app_id}"
+    end
+  end
 end
