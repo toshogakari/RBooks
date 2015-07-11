822e1a634564585606ff4f781dab806564e10b52
kazuhide maeda <kzhd15med3pub@gmail.com>
2015-07-11 17:40:58 +0900

    implements rspec

--- spec/rbooks/search_spec.rb
+++ spec/rbooks/search_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 
 RSpec.describe RBooks::Search do
   it 'searches the books of its mached books' do
-    stub_request(:any, 'app.rakuten.co.jp')
+    canned_response = File.new('./spec/features/rakutenbooks.json')
+    stub_request(:get,
+      'https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?applicationId=sample&format=json&title=%E3%81%93%E3%81%93%E3%82%8D'
+                ).to_return(body: canned_response)
 
     search = RBooks::Search.new('sample')
 
     result = search.get do |params|
-      params[:title] = 'Ruby'
+      params[:title] = 'こころ'
     end
 
-    expect(result).to eq('')
+    expect(2127).to eq(result['count'])
   end
 end
